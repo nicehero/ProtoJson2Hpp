b967ca4fb846d89747d9eab4b58cca1df203ce3e
nicehero <85948597@qq.com>
2019-07-09 15:35:47 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,27 @@
+
+#include "Message.h"
+#include <stdio.h>
+int main()
+{
+	Proto::TestType3 xxx;
+	Proto::TestType2 xxx2;
+	xxx2.test.myUi64 = 99999;
+	xxx2.myString = "hello world";
+	xxx.myNum = 666;
+	xxx.test.push_back(xxx2);
+	xxx.test.push_back(xxx2);
+	xxx.test.push_back(std::move(xxx2));
+	
+	//Serialize
+	nicehero::Message msg;
+	xxx.toMsg(msg);
+	
+	//Unserialize
+	Proto::TestType3 yyy;
+	msg >> yyy;
+	
+	printf("%s:%d\n",yyy.test[2].myString.c_str(),int(yyy.myNum));
+	
+	return 0;
+}
+
